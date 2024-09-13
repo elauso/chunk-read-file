dcabb49e61b915d54981473f739a45602c4fdeaa
elau <elauso@gmail.com>
2024-09-13 01:18:19 -0300

    ajuste do script init

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t chunk-read-file . && docker run \
 --network="host" --interactive --tty --rm \
---volume=./sample.csv:/data/sample.csv file-reader
+--volume=./sample.csv:/data/sample.csv chunk-read-file
